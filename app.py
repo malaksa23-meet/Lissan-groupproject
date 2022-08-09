14ef8ebc15e69256271e7b5e823c56d37f1e7741
malaksa23-meet <109904323+malaksa23-meet@users.noreply.github.com>
2022-08-09 16:48:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -126,6 +126,8 @@ def add_notes():
             return render_template("add_notes.html", error = error, all_notes=all_notes)
     else:
         all_notes=db.child("Notes").get().val()
+        if len(all_notes) == 0:
+            all_notes = ["No Notes"]
         return render_template("add_notes.html", all_notes=all_notes)
 
 
